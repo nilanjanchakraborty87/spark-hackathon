c2f28cbf58bd04a098372e923311700ee445b6d9
Tomasz Nurkiewicz <tomasz.nurkiewicz@4finance.com>
2014-11-28 15:15:30 +0100

    Day of week

--- src/main/java/Reports.java
+++ src/main/java/Reports.java
@@ -1,6 +1,5 @@
 import com.google.common.collect.Iterables;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
@@ -13,8 +12,18 @@ public class Reports {
 		SparkConf sparkConf = new SparkConf().setAppName("HackerNews");
 		sparkConf.setMaster("local");
 		JavaSparkContext spark = new JavaSparkContext(sparkConf);
+		groupByDayOfWeek(spark);
+	}
 
-		groupByYear(spark);
+	private static void groupByDayOfWeek(JavaSparkContext spark) {
+		spark.textFile("/home/tomasz/tmp/comments.csv")
+				.map(Comment::fromString)
+				.filter(Optional::isPresent)
+				.map(Optional::get)
+				.groupBy(comment -> comment.getCreatedAt().getDayOfWeek())
+				.map(pair -> new Tuple2<>(pair._1(), Iterables.size(pair._2())))
+				.collect()
+				.forEach(System.out::println);
 	}
 
 	private static void groupByYear(JavaSparkContext spark) {
@@ -24,14 +33,14 @@ public class Reports {
 				.map(Optional::get)
 				.groupBy(comment -> comment.getCreatedAt().getYear())
 				.map(pair -> new Tuple2<>(pair._1(), Iterables.size(pair._2()))).
-		saveAsTextFile("/home/tomasz/tmp/spark/by_year.txt");
+				saveAsTextFile("/home/tomasz/tmp/spark/by_year.txt");
 	}
 
 	private static void wordCount(JavaSparkContext spark) {
-		final JavaPairRDD<String, Integer> result = spark.textFile("/home/tomasz/tmp/comments.csv")
+		spark.textFile("/home/tomasz/tmp/comments.csv")
 				.flatMap(line -> Arrays.asList(line.split(" ")))
 				.mapToPair(s -> new Tuple2<>(s, 1))
-				.reduceByKey((a, b) -> a + b); result
+				.reduceByKey((a, b) -> a + b)
 				.saveAsTextFile("/home/tomasz/tmp/result.csv");
 	}
 
