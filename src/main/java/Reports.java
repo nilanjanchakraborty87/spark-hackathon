bc4be47fb29695a0008f4acb17c6e7a77ad3aac0
Tomasz Nurkiewicz <tomasz.nurkiewicz@4finance.com>
2014-11-28 16:04:25 +0100

    Hour of day

--- src/main/java/Reports.java
+++ src/main/java/Reports.java
@@ -1,47 +1,74 @@
 import com.google.common.collect.Iterables;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
-import java.util.Arrays;
-import java.util.Optional;
+import java.time.DayOfWeek;
+import java.util.List;
 
 public class Reports {
 
+	public static String input;
+
 	public static void main(String[] args) {
+		input = args[0];
 		SparkConf sparkConf = new SparkConf().setAppName("HackerNews");
 		sparkConf.setMaster("local");
 		JavaSparkContext spark = new JavaSparkContext(sparkConf);
-		groupByDayOfWeek(spark);
+		byHour(spark).forEach(System.out::println);
 	}
 
-	private static void groupByDayOfWeek(JavaSparkContext spark) {
-		spark.textFile("/home/tomasz/tmp/comments.csv")
-				.map(Comment::fromString)
-				.filter(Optional::isPresent)
-				.map(Optional::get)
+	private static List<Tuple2<Integer, Integer>> byLength(JavaSparkContext spark) {
+		return loadAndParse(spark)
+				.groupBy(comment -> comment.getCreatedAt().getHour())
+				.map(pair -> new Tuple2<>(pair._1(), Iterables.size(pair._2())))
+				.sortBy(Tuple2::_1, true, 1)
+				.collect();
+	}
+
+	private static List<Tuple2<Integer, Integer>> byHour(JavaSparkContext spark) {
+		return loadAndParse(spark)
+				.groupBy(comment -> comment.getCreatedAt().getHour())
+				.map(pair -> new Tuple2<>(pair._1(), Iterables.size(pair._2())))
+				.sortBy(Tuple2::_1, true, 1)
+				.collect();
+	}
+
+	private static List<Tuple2<String, Integer>> keywordTrend(JavaSparkContext spark, String keyword) {
+		return loadAndParse(spark)
+				.filter(c -> c.getCommentText().toLowerCase().contains(keyword))
+				.groupBy(Reports::monthKey)
+				.map(pair -> new Tuple2<>(pair._1(), Iterables.size(pair._2())))
+				.sortBy(Tuple2::_1, true, 1)
+				.collect();
+	}
+
+	private static String monthKey(Comment comment) {
+		return comment.getCreatedAt().getYear() + "-" + pad(comment.getCreatedAt().getMonthValue());
+	}
+
+	private static String pad(int value) {
+		return (value < 10)? ("0" + value) : ("" + value);
+	}
+
+	private static List<Tuple2<DayOfWeek, Integer>> groupByDayOfWeek(JavaSparkContext spark) {
+		return loadAndParse(spark)
 				.groupBy(comment -> comment.getCreatedAt().getDayOfWeek())
 				.map(pair -> new Tuple2<>(pair._1(), Iterables.size(pair._2())))
-				.collect()
-				.forEach(System.out::println);
+				.collect();
 	}
 
-	private static void groupByYear(JavaSparkContext spark) {
-		spark.textFile("/home/tomasz/tmp/comments.csv")
-				.map(Comment::fromString)
-				.filter(Optional::isPresent)
-				.map(Optional::get)
+	private static List<Tuple2<Integer, Integer>> groupByYear(JavaSparkContext spark) {
+		return loadAndParse(spark)
 				.groupBy(comment -> comment.getCreatedAt().getYear())
-				.map(pair -> new Tuple2<>(pair._1(), Iterables.size(pair._2()))).
-				saveAsTextFile("/home/tomasz/tmp/spark/by_year.txt");
+				.map(pair -> new Tuple2<>(pair._1(), Iterables.size(pair._2())))
+				.collect();
 	}
 
-	private static void wordCount(JavaSparkContext spark) {
-		spark.textFile("/home/tomasz/tmp/comments.csv")
-				.flatMap(line -> Arrays.asList(line.split(" ")))
-				.mapToPair(s -> new Tuple2<>(s, 1))
-				.reduceByKey((a, b) -> a + b)
-				.saveAsTextFile("/home/tomasz/tmp/result.csv");
+	private static JavaRDD<Comment> loadAndParse(JavaSparkContext spark) {
+		return spark.textFile(input)
+				.map(Comment::fromJson);
 	}
 
 }
